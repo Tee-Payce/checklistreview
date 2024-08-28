c7a78e0baa449892e90c30f4f5fb3ce99bfcc084
Tee-Payce <114886406+Tee-Payce@users.noreply.github.com>
2024-08-28 22:38:40 +0200

    Update wsgi.py

--- review/review/wsgi.py
+++ review/review/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 sys.path.append('C:\\Users\\sizibap\\Desktop\\checklistreview\\review')
